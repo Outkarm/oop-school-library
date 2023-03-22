4b29d52783caaab19c6e7a53a517c8cefad1effc
Outkarm <jkpordje@gmail.com>
2023-03-22 14:27:38 +0000

    dry code in person file

--- person.rb
+++ person.rb
@@ -2,23 +2,24 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(name = 'unknown', age, _parent_permission=true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def can_use_services?(is_of_age)
-   if is_of_age? || @parent_permission
+  def can_use_services?(_is_of_age)
+    return unless is_of_age? || @parent_permission
 
     true
   end
 
   private
 
-  def is_of_age?
-   if @age > 18
+  def of_age?
+    return unless @age > 18
+
     true
   end
 end
