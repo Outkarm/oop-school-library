7ce2f9dabe402e54ac2a49fe40f182aefe206ca7
Outkarm <jkpordje@gmail.com>
2023-03-25 10:37:42 +0000

    fix linter errors

--- person.rb
+++ person.rb
@@ -4,9 +4,8 @@ require './rental'
 class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age, :rental
-  
 
-  def initialize(age,rental, name = 'unknown', parent_permission: true)
+  def initialize(age, rental, name = 'unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
