4500ab410e18397448d0e5d04305af8cf76f3f87
Outkarm <jkpordje@gmail.com>
2023-03-25 12:35:30 +0000

    fix linter errors

--- main.rb
+++ main.rb
@@ -1,49 +1,65 @@
 require './app'
+
 class Main
+  MENU = {
+    '1' => :list_books,
+    '2' => :list_people,
+    '3' => :create_person,
+    '4' => :create_book,
+    '5' => :create_rental,
+    '6' => :list_rentals_for_person,
+    '7' => :quit
+  }.freeze
+
   def initialize
     @app = App.new
   end
 
   def display_menu
     puts 'Library App Menu:'
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a person'
-    puts '4. Create a book'
-    puts '5. Create a rental'
-    puts '6. List all rentals for a given person id'
-    puts '7. Quit the app'
+    MENU.each { |key, value| puts "#{key}. #{value.to_s.capitalize.gsub('_', ' ')}" }
   end
 
   def run
     loop do
       display_menu
       choice = gets.chomp
-      case choice
-      when '1'
-        @app.list_books
-      when '2'
-        @app.list_people
-      when '3'
-        @app.create_person
-      when '4'
-        @app.create_book
-      when '5'
-        @app.create_rental
-      when '6'
-        @app.list_rentals_for_person
-      when '7'
-        quit
-        break
+      if MENU.keys.include?(choice)
+        send(MENU[choice])
+        break if choice == '7'
       else
-        puts 'Invalid choice !. Please try again.'
+        puts 'Invalid choice! Please try again.'
       end
     end
+  end
+
+  def list_books
+    @app.list_books
+  end
+
+  def list_people
+    @app.list_people
+  end
+
+  def create_person
+    @app.create_person
+  end
+
+  def create_book
+    @app.create_book
+  end
+
+  def create_rental
+    @app.create_rental
+  end
+
+  def list_rentals_for_person
+    @app.list_rentals_for_person
   end
 
   def quit
     puts 'Thank you for using the Library App. Goodbye!'
   end
 end
-main = Main.new
-main.run
+
+Main.new.run
