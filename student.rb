4e01c02ccf2f144c219070e09af4862f1a28da12
Outkarm <jkpordje@gmail.com>
2023-03-25 09:41:19 +0000

    fix linter errors

--- student.rb
+++ student.rb
@@ -1,11 +1,10 @@
 require './classroom'
 require './person'
 
-
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'unknown', parent_permission = true)
+  def initialize(age, classroom, name = 'unknown', parent_permission: true)
     super(name, age, parent_permission)
     @classroom = classroom
   end
@@ -14,8 +13,8 @@ class Student < Person
     '¯(ツ)/¯'
   end
 
-  def classroom = (classroom)
+  def classroom=(classroom)
     @classroom = classroom
-    classroom.student.push(self) unless classroom.students.include?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
